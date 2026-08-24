3e72c455d47a0ec2c3340de4004a4e13a45f877d
MrSirJangles <beaustwrt248@gmail.com>
2026-08-24 09:07:51 +0800

    v2.5.0: add Firebase messaging service

---
+++ android/app/src/main/java/com/buysloans/hub/MorleyFirebaseMessagingService.kt
@@ -0,0 +1,22 @@
+package com.buysloans.hub
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MorleyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        DeviceRegistrar.register(this, token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        val title = message.notification?.title
+            ?: message.data["title"]
+            ?: "B&L Morley"
+        val body = message.notification?.body
+            ?: message.data["body"]
+            ?: "You have a new message."
+        NotificationHelper.showRemoteMessage(this, title, body)
+    }
+}
