3f01108710753e645c1068fd88adc86a6e8e72e9
MrSirJangles <beaustwrt248@gmail.com>
2026-08-24 09:08:15 +0800

    v2.5.0: register FCM token on app start

--- android/app/src/main/java/com/buysloans/hub/MorleyApplication.kt
+++ android/app/src/main/java/com/buysloans/hub/MorleyApplication.kt
@@ -1,11 +1,14 @@
 package com.buysloans.hub
 
 import android.app.Application
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MorleyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         NotificationHelper.createChannels(this)
         UpdateCheckScheduler.schedule(this)
+        FirebaseMessaging.getInstance().token
+            .addOnSuccessListener { token -> DeviceRegistrar.register(this, token) }
     }
 }
