30b200ed55a802232ee1578f579ca39eb7b99910
MrSirJangles <beaustwrt248@gmail.com>
2026-08-29 20:09:24 +0800

    Admin: install privacy-conscious crash telemetry (#257)

--- android/adminapp/src/main/java/com/buysloans/admin/AdminApplication.kt
+++ android/adminapp/src/main/java/com/buysloans/admin/AdminApplication.kt
@@ -5,6 +5,7 @@ import android.app.Application
 class AdminApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        AdminTelemetry.installCrashHandler(this)
         AdminCopyrightFooter.install(this)
     }
 }
