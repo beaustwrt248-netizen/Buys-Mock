9acbe8a8da56900e9947df2f4a77fd7bb2571e9a
MrSirJangles <beaustwrt248@gmail.com>
2026-09-03 07:55:43 +0800

    Fix pricing visual canvas imports

--- android/app/src/main/java/com/buysloans/hub/PricingVisuals.kt
+++ android/app/src/main/java/com/buysloans/hub/PricingVisuals.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
