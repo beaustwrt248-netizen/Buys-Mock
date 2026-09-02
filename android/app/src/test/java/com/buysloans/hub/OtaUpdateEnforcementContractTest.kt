157fa7632395d26ddb1b6de5a0eb1830ffc52918
MrSirJangles <beaustwrt248@gmail.com>
2026-09-02 21:09:54 +0800

    Use JUnit 4 in OTA enforcement contract test

--- android/app/src/test/java/com/buysloans/hub/OtaUpdateEnforcementContractTest.kt
+++ android/app/src/test/java/com/buysloans/hub/OtaUpdateEnforcementContractTest.kt
@@ -1,8 +1,8 @@
 package com.buysloans.hub
 
 import java.io.File
-import kotlin.test.Test
-import kotlin.test.assertTrue
+import org.junit.Assert.assertTrue
+import org.junit.Test
 
 class OtaUpdateEnforcementContractTest {
     @Test
